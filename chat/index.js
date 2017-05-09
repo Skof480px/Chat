fbd22488db91bbb6dd64a51662d0ed56d022afd4
Polina <sofi.skof@gmail.com>
2017-05-09 21:24:00 +0300

    init

---
+++ chat/index.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+var escape_html = require('escape-html');
+
+var server = http.createServer();
+var io = socketio(server);
+var port = 3000;
+
+fs.readFile('./index.html', function(err,html_string){
+	if(err) {
+		throw err;
+	}
+
+	io.on('connection', function(socket) {
+
+		socket.on('message', function (data) {
+			if(data && typeof data.nickname == 'string' && data.nickname && data.message)
+				socket.broadcast.emit('message', {nickname: escape_html(data.nickname), message: escape_html(data.message)})
+		})
+	})
+
+	server.on('request', function(request, response) {
+		response.writeHeader(200, {'Content-Type': 'text/html'});
+		response.end(html_string);
+	})
+
+	server.listen(port, function() {
+		console.log('Server running at port' + port);
+	})
+})
